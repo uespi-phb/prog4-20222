18a0700430c1ff9957191bcc71a4441aceadafc8
Eyder Rios <eyder.rios@gmail.com>
2023-06-06 17:56:49 -0300

    Aula 2023-06-06

---
+++ agenda/lib/models/contact.dart
@@ -0,0 +1,13 @@
+class Contact {
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+
+  Contact(
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
+  );
+}
--- agenda/lib/pages/contact_page.dart
+++ agenda/lib/pages/contact_page.dart
@@ -1,9 +1,53 @@
 import 'package:flutter/material.dart';
 
 import '../app/app_images.dart';
+import '../models/contact.dart';
 
-class ContactPage extends StatelessWidget {
-  const ContactPage({super.key});
+class ContactPage extends StatefulWidget {
+  final Contact? contact;
+
+  const ContactPage({
+    super.key,
+    this.contact,
+  });
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveContact() {
+    _formKey.currentState?.validate();
+  }
+
+  String? isNotEmpty(String? text) {
+    if ((text == null) || text.isEmpty) {
+      return 'Este campo é obrigatório';
+    }
+    return null;
+  }
+
+  String? isEmail(String? text) {
+    if (isNotEmpty(text) == null) {
+      if (!text!.contains('@')) {
+        return 'E-mail inválido';
+      }
+    }
+
+    return null;
+  }
+
+  String? isPhoneNumber(String? text) {
+    if (isNotEmpty(text) == null) {
+      if (int.tryParse(text!) == null) {
+        return 'Número de telefone inválido';
+      }
+    }
+
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +56,50 @@ class ContactPage extends StatelessWidget {
         title: const Text('Novo Contato'),
         centerTitle: true,
       ),
-      floatingActionButton: const FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.save),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _saveContact,
+        child: const Icon(Icons.save),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 100.0),
-            child: Image.asset(
-              AppImages.person,
-              width: 120.0,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80.0),
+              child: Image.asset(
+                AppImages.person,
+                width: 120.0,
+              ),
             ),
-          ),
-        ],
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    initialValue: 'Fulano de Tal',
+                    decoration: const InputDecoration(
+                      labelText: 'Nome',
+                    ),
+                    validator: isNotEmpty,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'E-mail',
+                    ),
+                    validator: isEmail,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Telefone',
+                    ),
+                    validator: isPhoneNumber,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
